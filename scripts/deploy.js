1d4ca6adb12e4354b818a8f876e3425681a7b5e7
Rio <onwukachibike@gmail.com>
2023-04-06 16:28:11 +0100

    created deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,26 @@
+async function main() {
+
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+    // deploy contracts here:
+    const NFT = await ethers.getContractFactory("NFT");
+    const nft = await NFT.deploy();
+  
+    // deploy contracts here:
+    const Marketplace = await ethers.getContractFactory("Marketplace");
+    const marketplace = await Marketplace.deploy(1);
+  
+    console.log("NFT contract address", nft.address)
+    console.log("Marketplace contract address", marketplace.address)
+  
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
