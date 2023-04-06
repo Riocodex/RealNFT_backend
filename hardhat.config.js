c5d8b36b672a7c58183ddd4c6c7c6cae063e01d5
Rio <onwukachibike@gmail.com>
2023-04-06 16:48:03 +0100

    compiled

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,16 +4,7 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
-  networks: {
-    mumbai:{
-      url: process.env.TESTNET_RPC_KEY,
-      accounts:[process.env.PRIVATE_KEY]
-    },
-    goerli: {
-      url: process.env.TESTNET_RPC_KEY,
-      accounts: [process.env.PRIVATE_KEY]
-    }
-  },
+  
   etherscan:{
     apiKey:process.env.POLYGONSCAN_API_KEY
   }
